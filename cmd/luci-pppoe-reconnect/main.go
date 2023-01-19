e6770944aa5fb3c85de5fc8a028370b7466663e7
117503445 <t117503445@gmail.com>
2023-01-19 14:07:36 +0000

    feat: config
    Signed-off-by: 117503445 <t117503445@gmail.com>

--- cmd/luci-pppoe-reconnect/main.go
+++ cmd/luci-pppoe-reconnect/main.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/117503445/luci-pppoe-reconnect/internal/cfg"
 	"github.com/117503445/luci-pppoe-reconnect/internal/log"
+	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
 	// This controls the maxprocs environment variable in container runtimes.
 	// see https://martin.baillie.id/wrote/gotchas-in-the-go-network-packages-defaults/#bonus-gomaxprocs-containers-and-the-cfs
 	_ "go.uber.org/automaxprocs"
+
+	"github.com/imroc/req/v3"
 )
 
 func main() {
@@ -29,7 +33,13 @@ func run() error {
 		err = logger.Sync()
 	}()
 
-	logger.Info("Hello world!", zap.String("location", "world"))
+	cfg.InitConfig()
+
+	logger.Info("Hello world!", zap.String("connector.name", viper.GetString("connector.name")))
+
+	client := req.C().DevMode()
+	_, err = client.R().Post("http://httpbin.org/get")
+	// fmt.Println(resp)
 
 	return err
 }
---
+++ internal/cfg/config.go
@@ -0,0 +1,17 @@
+package cfg
+
+import (
+	"github.com/spf13/viper"
+)
+
+func InitConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("./manifest/config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
